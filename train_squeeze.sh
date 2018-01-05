c8f75104b995c2158d3662300223e4e1ebadab54
christopheryin <chhyin136@gmail.com>
2018-01-05 13:05:43 -0800

    new model

--- train_squeeze.sh
+++ train_squeeze.sh
@@ -1,1 +1,1 @@
-python train_squeezenet.py --train_dir train/train_dir --val_dir train/val_dir --nb_epoch 5 --batch_size 64 --output_model_file "sand_model4.h5" --plot
+python train_squeezenet.py --train_dir train/train_dir --val_dir train/val_dir --nb_epoch 7 --batch_size 64 --output_model_file "sand_model5.h5" --plot
