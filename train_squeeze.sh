84e67e8b2fedef851e756ee815d80e848f5d2644
christopheryin <chhyin136@gmail.com>
2018-01-05 13:36:28 -0800

    model5 with .839 accuracy

--- train_squeeze.sh
+++ train_squeeze.sh
@@ -1,1 +1,1 @@
-python train_squeezenet.py --train_dir train/train_dir --val_dir train/val_dir --nb_epoch 7 --batch_size 64 --output_model_file "sand_model5.h5" --plot
+python train_squeezenet.py --train_dir train/train_dir --val_dir train/val_dir --nb_epoch 8 --batch_size 128 --output_model_file "sand_model6.h5" --plot
